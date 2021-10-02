02db0eea8ba2d3726880a467aa51c9c5b943e6da
Kemas Rafly Omar Thoriq <rafly.badry20@gmail.com>
2021-10-02 10:56:15 +0700

    CS 4 mengedit sedikit filter

--- kemasJmartAK/Filter.java
+++ kemasJmartAK/Filter.java
@@ -12,23 +12,23 @@ public class Filter
 {
     public static ArrayList<PriceTag> filterPriceTag(PriceTag[] list, double value, boolean less) 
     {
-        ArrayList<PriceTag> priceTags = new ArrayList<>();
-        for (PriceTag e : list) {
-            if (less && e.getAdjustedPrice() < value || !less && e.getAdjustedPrice() >= value)
+        ArrayList<PriceTag> priceTag = new ArrayList<>();
+        for (PriceTag s : list) {
+            if (less && s.getAdjustedPrice() < value || !less && s.getAdjustedPrice() >= value)
             {
-                priceTags.add(e);
+                priceTag.add(s);
             }
         }
         
-        return priceTags;
+        return priceTag;
     }
 
     public static void filterProductRating(ArrayList<ProductRating> list, double value, boolean less) 
     {
         for (int i = 0; i < list.size(); ++i) 
         {
-            final ProductRating e = list.get(i);
-            if (less && e.getAverage() < value || !less && e.getAverage() >= value)
+            final ProductRating s = list.get(i);
+            if (less && s.getAverage() < value || !less && s.getAverage() >= value)
             {
                 list.remove(i);
             }
