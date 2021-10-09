4f5b6aa429aba15631de3ec5537044c43e341e29
Kemas Rafly Omar Thoriq <rafly.badry20@gmail.com>
2021-10-09 11:22:10 +0700

    CS Modul 5 (Benerin Class Algorithm)

--- kemasJmartAK/Algorithm.java
+++ kemasJmartAK/Algorithm.java
@@ -1,162 +1,94 @@
 package kemasJmartAK;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.function.Predicate;
 
 public class Algorithm {
 
 	private Algorithm() {
-
-	}
-
-	public static <T> int count(T[] array, T value) {
-		int counter = 0;
-		for (T e : array) {
-			counter++;
-		}
-		return counter;
-	}
-
-	public static <T> int count(Iterable<T> iterable, T value) {
-		int counter = 0;
-		for (T e : iterable) {
-			counter++;
-		}
-		return counter;
-	}
-
-	public static <T> int count(Iterator<T> iterator, T value) {
-		int counter = 0;
-
-		if (iterator.hasNext()) {
-			counter++;
-		}
-		return counter;
-	}
-
-	@SuppressWarnings("unlikely-arg-type")
-	public static <T> int count(T[] array, Predicate<T> pred) {
-		if (pred.equals(false)) {
-			return 0;
-		}
-
-		int counter = 0;
-		for (T e : array) {
-			counter++;
-		}
-		return counter;
-	}
-
-	@SuppressWarnings("unlikely-arg-type")
-	public static <T> int count(Iterable<T> iterable, Predicate<T> pred) {
-		if (pred.equals(false)) {
-			return 0;
-		}
-
-		int counter = 0;
-		for (T e : iterable) {
-			counter++;
-		}
-		return counter;	
-	}
-
-	@SuppressWarnings("unlikely-arg-type")
-	public static <T> int count(Iterator<T> iterator, Predicate<T> pred) {
-		if (pred.equals(false)) {
-			return 0;
-		}
-
-		int counter = 0;
-
-		if (iterator.hasNext()) {
-			counter++;
-		}
-		return counter;
-	}
-
-	public static <T> boolean exists(T[] array, T value) {
-		for (T e : array) {
-			if (e.equals(value)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public static <T> boolean exists(Iterable<T> iterable, T value) {
-		for (T e : iterable) {
-			if (e.equals(value)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public static <T> boolean exists(Iterator<T> iterator, T value) {
-		while (iterator.hasNext()) {
-			if (iterator.next().equals(value)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public static <T> boolean exists(T[] array, Predicate<T> pred) {
-		for (T e : array) {
-			if (e.equals(pred)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public static <T> boolean exists(Iterable<T> iterable, Predicate<T> pred) {
-		for (T e : iterable) {
-			if (e.equals(pred)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public static <T> boolean exists(Iterator<T> iterator, Predicate<T> pred) {
-		while (iterator.hasNext()) {
-			if (iterator.next().equals(pred)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public static <T> T find(T[][] array, T value) {
-		return null;
-	}
-
-	public static <T> T find(Iterable<T> iterable, T value) {
-		return null;
-	}
-
-	public static <T> T find(Iterator<T> iterator, T value) {
-		return null;
-	}
-
-	public static <T> T find(T[][] array, Predicate<T> pred) {
-		return null;
-	}
-
-	public static <T> T find(Iterable<T> iterable, Predicate<T> pred) {
-		return null;
-	}
-
-	public static <T> T find(Iterator<T> iterator, Predicate<T> pred) {
-		return null;
-	}
-
-	public static <T> T max(T first, T second) {
-		return null;
-	}
-
-	public static <T> T min(T first, T second) {
-		return null;
-	}
+		  
+	 }
+	 
+	    public static <T> int count(T[] array,T value){
+	  final Iterator<T> check = Arrays.stream(array).iterator();
+	  return count(check, value);
+	    }
+	    public static <T> int count(Iterable<T> iterable, T value){
+	  final Iterator <T> check = iterable.iterator();
+	  return count(check, value);
+	    }
+	 public static <T> int count(Iterator<T> iterator, T value){
+	  final Predicate <T> pred = value::equals;
+	  return count(iterator,pred);
+	 }
+	 public static <T> int count(T[] array, Predicate<T> pred){
+	  final Iterator<T> check = Arrays.stream(array).iterator();
+	  return count(check, pred);
+	 }
+	 public static <T> int count(Iterable<T> iterable, Predicate<T> pred){
+	  final Iterator<T> check = iterable.iterator();
+	  return count(check, pred);
+	 }
+	 public static <T> int count(Iterator<T> iterator, Predicate<T> pred){
+	  return count(iterator, pred);
+	 }
+	 
+	 
+	 public static <T> boolean exists(T[] array, T value) {
+	  final Iterator<T> check = Arrays.stream(array).iterator();
+	  return exists(check, value);
+	 }
+	 public static <T> boolean exists(Iterable<T> iterable, T value){
+	  final Iterator<T> check = iterable.iterator();
+	  return exists(check, value);
+	 }
+	 public static <T> boolean exists(Iterator<T> iterator, T value){
+	  final Predicate <T> pred = value::equals;
+	  return exists(iterator,pred);
+	 }
+	 public static <T> boolean exists(T[] array, Predicate<T> pred){
+	  final Iterator<T> check = Arrays.stream(array).iterator();
+	  return exists(check, pred);
+	 }
+	 public static <T> boolean exists(Iterable<T> iterable, Predicate<T> pred){
+	  final Iterator <T> check = iterable.iterator();
+	  return exists(check, pred);
+	 }
+	 public static <T> boolean exists(Iterator<T> iterator, Predicate<T> pred){
+	  return exists(iterator, pred);
+	 }
+	 
+	 
+	 public static <T> T find(T[] array, T value) {
+	  final Iterator<T> check = Arrays.stream(array).iterator();
+	  return find(check, value);
+	 }
+	 public static <T> T find(Iterable<T> iterable, T value) {
+	  final Iterator <T> check = iterable.iterator();
+	  return find(check, value);
+	 }
+	 public static <T> T find(Iterator<T> iterator, T value) {
+	  final Predicate <T> pred = value::equals;
+	  return find(iterator, pred);
+	 }
+	 public static <T> T find(T[] array, Predicate<T> pred) {
+	  final Iterator<T> check = Arrays.stream(array).iterator();
+	  return find(check, pred);
+	 }
+	 public static <T> T find(Iterable<T> iterable, Predicate<T> pred) {
+	  final Iterator <T> check = iterable.iterator();
+	  return find(check, pred);
+	 }
+	 public static <T> T find(Iterator<T> iterator, Predicate<T> pred) {
+	  return find(iterator, pred);
+	 }
+	 
+	 
+	 public static <T> T max(T first, T second) {
+	  return null;
+	 }
+	 public static <T> T min(T first, T second) {
+	  return null;
+	 }
 }
--- kemasJmartAK/Recognizable.java
+++ kemasJmartAK/Recognizable.java
@@ -44,11 +44,6 @@ public class Recognizable implements Comparable<Recognizable>
 
     @Override
     public int compareTo(Recognizable other) {
-        if (id == other.id){
-            return 1;
-        }
-        else{
-            return 0;
-        }
+        return Integer.compare(this.id, other.id);
     }
 }
